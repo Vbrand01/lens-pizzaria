efccb48c979a3d606d4361846d6216eb605d92ef
Vbrand01 <viccorin20@gmail.com>
2023-01-03 09:54:16 -0300

    add nav+banner+footer

---
+++ src/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="main-banner">
+        <h1>Lens Pizzaria</h1>
+        <!-- <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium velit earum amet aliquid est obcaecati eos placeat ex corrupti. Nostrum distinctio cupiditate sed in quo beatae expedita impedit nemo tempore?</p> -->
+        <!-- <img src="../../public/img/3d.png" alt=""> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Banner"
+    }
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('../../public/img/pizzahome.png');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 700px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        color: rgb(221, 221, 221);
+        text-align: center;
+        font-size: 60px;
+        background-color: rgb(128, 1, 1);
+        padding: 20px 40px;
+        border-radius: 8px;
+        margin: 200px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte a sua pizza:</h1>
+    <p>formulario</p>
   </div>
 </template>
 
 <script>
+
+  import Banner from '../components/Banner.vue';
+
+  export default {
+    name: "Home",
+    components: {
+      Banner
+    }
+  }
 
 </script>
